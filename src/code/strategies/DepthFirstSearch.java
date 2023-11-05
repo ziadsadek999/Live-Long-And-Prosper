9ad7330137d8e7c70d4fe430d3d6ba32ff8d690d
shimaabetah <shimaabetah1911@gmail.com>
2023-11-05 17:07:06 +0200

    Implement DFS Strategy
    Co-authored-by: Ahmed Elwasefi <ahmad.alwasifi@gmail.com>

--- src/code/strategies/DepthFirstSearch.java
+++ src/code/strategies/DepthFirstSearch.java
@@ -1,5 +1,10 @@
 package code.strategies;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
 import code.artifacts.Node;
 
 public class DepthFirstSearch extends GenericSearch {
@@ -7,4 +12,29 @@ public class DepthFirstSearch extends GenericSearch {
     public DepthFirstSearch(Node root) {
         super(root);
     }
+
+    public Node solve(){
+        HashSet<String> explored = new HashSet<>();
+        Stack<Node> stack = new Stack<>();
+
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            if (node.isGoal()) {
+                return node;
+            }
+            if (explored.contains(node.toString())) {
+                continue;
+            }
+            explored.add(node.toString());
+            List<Node> children = expand(node);
+            for (Node child : children) {
+                if(!explored.contains(child.toString())){
+                    stack.push(child);
+                }
+            }
+        }
+        return null;
+    }
+
 }
